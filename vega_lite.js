5056f4be42bd278815119296c97d721b5f5e0108
Abdullahi104 <aali0022@student.monash.edu>
2023-10-13 00:01:54 +1100

    Week 10 HW

--- vega_lite.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "my_chlropleth_map.json";
-vegaEmbed("#map1", vg_1).then(function(result) {
-// Access the Vega view instance https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
